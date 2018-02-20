29ebd37f39b184e2fbaa3edf4ca8410bc161f492
Casey McLaughlin <caseyamcl@gmail.com>
2018-02-19 21:20:35 -0500

    Twig Factory works now.

--- src/TwigFactory.php
+++ src/TwigFactory.php
@@ -3,8 +3,11 @@
 namespace Iggy;
 
 use Psr\Http\Message\RequestInterface;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
+use Twig_Loader_Array;
 
 /**
  * Class TwigFactory
@@ -12,11 +15,6 @@ use Twig\Loader\FilesystemLoader;
  */
 class TwigFactory
 {
-    /**
-     * @var array|string[]
-     */
-    private $paths;
-
     /**
      * @var IggyTwigExtension
      */
@@ -30,10 +28,9 @@ class TwigFactory
     public function __construct(array $paths)
     {
         $this->paths = $paths;
-        $this->twig = new Environment(new FilesystemLoader($this->paths), ['debug' => true]);
-        $this->twigExtension = new IggyTwigExtension();
-        $this->twig->addExtension($this->twigExtension);
     }
+
+
 
     /**
      * @param RequestInterface $request
@@ -41,7 +38,30 @@ class TwigFactory
      */
     public function getTwigEnvironment(RequestInterface $request): Environment
     {
-        $this->twigExtension->setRequest($request);
-        return $this->twig;
+        // Thanks, https://github.com/CouscousPHP/Couscous/
+        // But.. this loads all file content into memory for every request.  Very memory inefficient.
+        // TODO: Figure out something better.
+        $finder = new Finder();
+        $finder->files()->in($this->paths)->name('*.twig');
+        $layouts = [];
+
+        foreach ($finder as $file) {
+            /** @var SplFileInfo $file */
+            $name = $file->getFilename();
+            $layouts[$name] = $file->getContents();
+        }
+
+        $loader = new Twig_Loader_Array($layouts);
+
+        $twig = new Environment($loader, [
+            'debug'       => true,
+            'auto_reload' => true,
+            'cache'       => false
+        ]);
+
+        $twigExtension = new IggyTwigExtension($request);
+        $twig->addExtension($twigExtension);
+
+        return $twig;
     }
 }
